fa752a9155901fd03dfbeda2856109dc567b3a9f
Raghu Nandan <144773863+Raghu6798@users.noreply.github.com>
2025-04-16 19:28:33 +0530

    Update redis_cache.py

--- databases/Redis/redis_cache.py
+++ databases/Redis/redis_cache.py
@@ -1,17 +1,13 @@
 import os 
-import sys 
+
 
 import redis
 from dotenv import load_dotenv
 from langchain_redis.cache import RedisSemanticCache
 
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..')))
-
-from agent2agent.models.embedding_model.embed_model import Embed_model
+from models.embedding_model.embed_model import Embed_model
 
 load_dotenv()
-
-
 
 semantic_cache = RedisSemanticCache(
     redis_url=os.getenv("REDIS_URI"), embeddings=Embed_model, distance_threshold=0.2
